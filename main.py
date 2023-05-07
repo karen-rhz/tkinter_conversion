cc122822204c91a576be92517b5cdf7d33beef98
karen-rhz <127526793+karen-rhz@users.noreply.github.com>
2023-05-07 15:16:22 -0400

    Create layout

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+import tkinter
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+window = tkinter.Tk()
+window.title("First GUI Program")
+window.minsize(500, 500)
+window.config(padx=20, pady=20)
+
+# Label : text in the window
+label_1 = tkinter.Label(text="Convert Miles to Km", font=("Space Mono", 24))
+label_1.grid(column=1, row=1)
+
+# # To change the text on screen
+# label_1['text'] = "Another text"
+# # or
+# label_1.config(text="Another text")
+
+# Entry : Receive input
+user_input = tkinter.Entry(width=30)
+user_input.grid(column=1, row=2)
+
+label_2 = tkinter.Label(text="Km", font=("Space Mono", 24))
+label_2.grid(column=2, row=2)
+
+label_3 = tkinter.Label(text="is equal to", font=("Space Mono", 24))
+label_3.grid(column=1, row=3)
+
+label_4 = tkinter.Label(text="RESULT", font=("Space Mono", 24))
+label_4.grid(column=1, row=4)
+
+label_5 = tkinter.Label(text="Miles", font=("Space Mono", 24))
+label_5.grid(column=2, row=4)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Button
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+window.mainloop()
